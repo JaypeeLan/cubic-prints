0bc4f23a8f62ed70d248721e006dd7973d4f3622
Laniran JohnPaul <jplaniran01@gmail.com>
2024-12-27 12:23:59 +0100

    update brands images

--- src/components/Sections/Brands.tsx
+++ src/components/Sections/Brands.tsx
@@ -1,19 +1,26 @@
-import mtn from "../../assets/mtn.png";
-import airtel from "../../assets/airtel.png";
+import brand1 from "../../assets/brand1.png";
+import brand2 from "../../assets/brand2.png";
+import brand3 from "../../assets/brand3.png";
+import brand4 from "../../assets/brand4.png";
+import brand5 from "../../assets/brand5.png";
+import brand6 from "../../assets/brand6.png";
+import brand7 from "../../assets/brand7.png";
+import brand8 from "../../assets/brand8.png";
+
 const Brands = () => {
   return (
     <section className="brand" data-aos="fade-up">
       <div className="brands">
         <p className="brands-title">Brands Highlight</p>
         <div className="brands-imgs">
-          <img src={airtel} alt="airtel" width={128} height={32} />
-          <img src={mtn} alt="mtn" width={64} height={32} />
-          <img src={airtel} alt="airtel" width={128} height={32} />
-          <img src={mtn} alt="mtn" width={64} height={32} />
-          <img src={airtel} alt="airtel" width={128} height={32} />
-          <img src={mtn} alt="mtn" width={64} height={32} />
-          <img src={airtel} alt="airtel" width={128} height={32} />
-          <img src={mtn} alt="mtn" width={64} height={32} />
+          <img src={brand1} alt="airtel" width={158} height={54} />
+          <img src={brand2} alt="mtn" width={158} height={54} />
+          <img src={brand3} alt="airtel" width={158} height={54} />
+          <img src={brand4} alt="mtn" width={158} height={54} />
+          <img src={brand5} alt="airtel" width={158} height={54} />
+          <img src={brand6} alt="mtn" width={158} height={54} />
+          <img src={brand7} alt="airtel" width={158} height={54} />
+          <img src={brand8} alt="mtn" width={158} height={54} />
         </div>
       </div>
     </section>
