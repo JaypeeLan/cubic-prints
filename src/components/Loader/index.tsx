fa6c7d9cc56e10c341c6406bfb71e0983af96e96
Laniran JohnPaul <jplaniran01@gmail.com>
2025-02-28 07:31:16 +0100

    update loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -4,7 +4,13 @@ import Logo from "../../assets/logoWhite.png";
 const Loader = () => {
   return (
     <div className="loader-container">
-      <img src={Logo} alt="Loading..." className="loader-logo" />
+      <img
+        src={Logo}
+        alt="Loading..."
+        className="loader-logo"
+        width={"100%"}
+        height={"100%"}
+      />
     </div>
   );
 };
