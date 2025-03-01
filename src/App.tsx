659576e2628fccedff77a95329c2857713a56859
Laniran JohnPaul <jplaniran01@gmail.com>
2025-03-01 17:51:26 +0100

    add versel json

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,5 @@
-import Header from "./components/Header";
-import Cta from "./components/Sections/Cta";
-import Discover from "./components/Sections/Discover";
-import FAQ from "./components/Sections/FAQ";
-import Footer from "./components/Sections/Footer";
-import Hero from "./components/Sections/Hero";
-import Mission from "./components/Sections/Mission";
-import Packaging from "./components/Sections/Packaging";
-import Services from "./components/Sections/Services";
-import Showcase from "./components/Sections/Showcase";
-import Testimonials from "./components/Sections/Testimonials";
-import "./App.css";
-import Brands from "./components/Sections/Brands";
-import SEO from "./components/SEO";
-import AnimatedWrapper from "./components/AnimatedWrapper";
-
 function App() {
-  return (
-    <>
-      <SEO />
-      <Header />
-      <AnimatedWrapper>
-        <>
-          <Hero />
-          <Mission />
-          <Services />
-          <Packaging />
-          <Discover />
-          <Showcase />
-          <Brands />
-          <Testimonials />
-          <Cta />
-          <FAQ />
-        </>
-      </AnimatedWrapper>
-      <Footer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
